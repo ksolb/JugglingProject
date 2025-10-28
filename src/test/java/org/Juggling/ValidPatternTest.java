f2f13ae1e953fed6c5d2d33787b73a837b6a4f42
ksolb <162927220+ksolb@users.noreply.github.com>
2025-10-27 23:12:57 -0400

    Added Valid and Invalid pattern tests

--- src/test/java/org/Juggling/ValidPatternTest.java
+++ src/test/java/org/Juggling/ValidPatternTest.java
@@ -7,14 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ValidPatternTest {
 
+    ValidPattern vp = new ValidPattern();
+    String input = "";
     @Test
-    void isValidPatternTest() {
-        ValidPattern vp = new ValidPattern();
-        String input = "531";
+    void validPatternTest() {
+        input = "531";
         int result = vp.isValidPattern(input);
-
         Assertions.assertEquals(1, result);
-
+    }
+    @Test
+    void invalidPatternTest() {
+        input = "513";
+        int result = vp.isValidPattern(input);
+        Assertions.assertEquals(-1, result);
     }
 
 }
